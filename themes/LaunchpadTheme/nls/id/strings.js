ae36a9a1e7d246efe5d1cbf83484efb09d86e0be
anmaro13 <andrmartinro@gmail.com>
2017-04-20 17:09:49 +0200

    Version1

---
+++ themes/LaunchpadTheme/nls/id/strings.js
@@ -0,0 +1,5 @@
+define({
+  "_themeLabel": "Tema Launchpad",
+  "_layout_default": "Tata Letak Default",
+  "_layout_right": "Tata Letak Kanan"
+});
---
+++ themes/LaunchpadTheme/widgets/AnchorBarController/nls/id/strings.js
@@ -0,0 +1,6 @@
+define({
+  "_widgetLabel": "Pengontrol Anchor Bar",
+  "_layout_default": "Tata Letak Default",
+  "_layout_layout1": "Tata Letak 0",
+  "more": "Widget Lainnya"
+});
